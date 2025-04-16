cee68c10c863521257b3522c326b6f8402a5aace
Luka Chitaishvili <lchitaishvili@gmail.com>
2025-04-16 18:23:07 +0400

    fix expanded nav issue

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -21,6 +21,7 @@ export class HeaderComponent implements OnInit {
       tap(event => {
         if (event instanceof NavigationEnd) {
           this.isHome = event.url === '/';
+          this.isNavExpanded = false;
         }
       })
     ).subscribe();
