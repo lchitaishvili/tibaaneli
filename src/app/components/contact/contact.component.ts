76e13a4af657380de6e6dd8a855fa202e4b2733f
Luka Chitaishvili <lchitaishvili@gmail.com>
2025-04-16 15:09:55 +0400

    feat: set up contact form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import emailjs from 'emailjs-com';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: 'contact.component.html',
   styleUrl: 'contact.component.scss'
 })
-export class ContactComponent {}
+export class ContactComponent {
+  public form = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl('', Validators.email),
+    message: new FormControl(''),
+  });
+
+  sendEmail(): void {
+    if (this.form.valid) {
+      emailjs.send(
+        'service_lletywd',
+        'template_oyfrvbb',
+        this.form.value,
+        'qqkHqMQQyNI5cgt_Z'
+      ).then();
+      this.form.reset();
+      alert('Message Sent Successfully');
+    }
+  }
+}
+
