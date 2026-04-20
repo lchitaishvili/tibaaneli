be313c18d96052e66cd6f90f18296726c37e3aae
Luka Chitaishvili <lchitaishvili@gmail.com>
2026-04-21 02:08:12 +0400

    feat: enhance preload behavior and accessibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import {AfterViewInit, ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {FooterComponent} from "./components/footer/footer.component";
 import {HeaderComponent} from "./components/header/header.component";
-import {changeAttribute, changeClass, delay, getElement} from "./helpers";
 import * as AOS from 'aos';
 
 @Component({
@@ -13,18 +12,57 @@ import * as AOS from 'aos';
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
   title = 'tibaaneli';
+  isPreloadHidden = false;
+
+  private hidePreloadTimeoutId: ReturnType<typeof setTimeout> | null = null;
+  private onWindowLoad = () => this.scheduleHidePreload();
+
+  constructor(private readonly cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     AOS.init({
       duration: 800,
       easing: 'ease-in-sine'
     });
+  }
 
-    const preload = getElement('#preload');
-    window.addEventListener('load', () => {
-      delay(() => changeClass(preload, 'null', 'loaded'), 400);
-    });
+  ngAfterViewInit() {
+    if (document.readyState === 'complete') {
+      this.scheduleHidePreload();
+      return;
+    }
+
+    window.addEventListener('load', this.onWindowLoad, { once: true });
+
+    // Never block the app forever when some resources are too slow.
+    this.hidePreloadTimeoutId = setTimeout(() => this.hidePreload(), 8000);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('load', this.onWindowLoad);
+    this.clearFallbackTimer();
+  }
+
+  private scheduleHidePreload() {
+    setTimeout(() => this.hidePreload(), 400);
+  }
+
+  private hidePreload() {
+    if (this.isPreloadHidden) {
+      return;
+    }
+
+    this.isPreloadHidden = true;
+    this.clearFallbackTimer();
+    this.cdr.markForCheck();
+  }
+
+  private clearFallbackTimer() {
+    if (this.hidePreloadTimeoutId !== null) {
+      clearTimeout(this.hidePreloadTimeoutId);
+      this.hidePreloadTimeoutId = null;
+    }
   }
 }
